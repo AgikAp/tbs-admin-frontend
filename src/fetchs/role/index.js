685e2a2fb724820db05ae4d4045665ef11c9fad8
agik.ap <agik2173@gmail.com>
2024-07-16 17:31:48 +0700

    Add endpoint to update and create role

--- src/fetchs/role/index.js
+++ src/fetchs/role/index.js
@@ -1,9 +1,24 @@
 import instance from "../../axios"
 
-export const GET_RoleList = async (setLoading) => {
+export const GET_RoleList = async (setLoading, withAccess) => {
   setLoading(true)
   try {
-    const response = await instance.get('/role')
+    var url = withAccess ? '/role?access='.concat(withAccess) : '/role'
+    const response = await instance.get(url)
+    setLoading(false)
+    return response
+  } catch (e) {
+    setLoading(false)
+    throw e
+  }
+
+}
+
+export const POST_RoleCreateOrUpdate = async (setLoading, payload, update) => {
+  setLoading(true)
+  try {
+    var url = update ? '/role?update' : '/role'
+    const response = await instance.post(url, payload)
     setLoading(false)
     return response
   } catch (e) {
--- src/pages/roleaccess/index.jsx
+++ src/pages/roleaccess/index.jsx
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import PageHeader from '../../components/pageheader'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faSave, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import Section from '../../components/sections'
 import { GET_AccessList } from '../../fetchs/access'
+import { GET_RoleList, POST_RoleCreateOrUpdate } from '../../fetchs/role'
+import InputLabel from '../../components/inputlabel'
+import RoleItem from './roleItem'
+import { errorWriter } from '../../utils/errorwriter'
 
 export default function RoleAccessPage() {
+  const [isEdit, setIsEdit] = useState(false)
   const [loading, setLoading] = useState(false)
   const [err, setErr] = useState(null)
 
-  const [accessList, setAccessList] = useState([])
+  const [activeRole, setActiveRole] = useState({})
+  const [detailRole, setDetailRole] = useState({})
+  const [accessActive, setAccessActive] = useState([])
 
-  const additionalRoleAccess = (
-    <>
-      <button className='btn bg-primary-2 hover:bg-primary-1 text-light-0 font-semibold px-5 lg:px-7 text-[14px]' onClick={() => ''}>
-        <FontAwesomeIcon icon={faPlus} />
-        Add New
-      </button>
-    </>
-  )
+  const [accessList, setAccessList] = useState([])
+  const [listRole, setListRole] = useState([])
 
   const fetchAccessList = async () => {
     try {
@@ -29,9 +30,85 @@ export default function RoleAccessPage() {
     }
   }
 
+  const fetchRoleList = async () => {
+    try {
+      const response = await GET_RoleList(setLoading, 'structural')
+      setListRole(response.data.data)
+    } catch (e) {
+      errorWriter(e, setErr)
+    }
+  }
+  
+  const postRoleUpdateOrCreate = async () => {
+    try {
+      var payload = {...detailRole}
+      
+      const response = await POST_RoleCreateOrUpdate(setLoading, detailRole, isEdit)
+      setListRole(response.data.data)
+    } catch (e) {
+      errorWriter(e, setErr)
+    }
+  }
+
   useEffect(() => {
     fetchAccessList()
+    fetchRoleList()
   }, [])
+
+  const recursiveSetRole = (access) => {
+    var accesses = [access]
+    for (var i = 0; i < access?.accesses?.length; i++) {
+      var insideAccess = access?.accesses[i]
+      accesses.push(insideAccess)
+      if (insideAccess?.accesses > 0) {
+        var additionalAccess = recursiveSetRole(insideAccess)
+        accesses.push(...additionalAccess)
+      }
+    }
+
+    return accesses
+  }
+
+  useEffect(() => {
+    var accesses = []
+    activeRole?.role_accesses?.forEach(val => {
+      var additionalAccess = recursiveSetRole(val)
+      accesses.push(...additionalAccess)
+    })
+
+    setAccessActive(accesses)
+  }, [activeRole])
+
+  const showModal = (val, edit) => {
+    var newVal = { ...val }
+    if (!edit) {
+      newVal.parent_id = newVal?.id
+      newVal.id = ''
+      newVal.name = ''
+    } else {
+      newVal.before_id = newVal?.id
+    }
+
+    setIsEdit(edit)
+    setDetailRole(newVal)
+    document.getElementById('modal_detail').showModal();
+  };
+
+  const closeModal = (id) => {
+    document.getElementById('modal_detail').close()
+  }
+
+  const forceUpperSlug = (value) => {
+    return value.toUpperCase().replaceAll(' ', '_')
+  }
+
+  const onSubmit = async () => {
+    await postRoleUpdateOrCreate()
+    closeModal()
+    fetchRoleList()
+    setActiveRole({})
+    setDetailRole({})
+  }
 
   return (
     <>
@@ -41,71 +118,62 @@ export default function RoleAccessPage() {
           <span>{err}</span>
         </div>}
 
+      <dialog id="modal_detail" className="modal">
+        <div className="modal-box">
+          {
+            isEdit ?
+              <h3 className="font-bold text-lg">Edit {detailRole?.id}</h3> :
+              <h3 className="font-bold text-lg">Add New</h3>
+
+          }
+          <div className=''>
+            <InputLabel label={'Parent'} name={'parent'} value={detailRole?.parent_id ?? '-'} readOnly={true} />
+            <InputLabel label={'ID'} name={'id'} value={detailRole?.id ?? ''} readOnly={true} />
+            <InputLabel label={'Name'} name={'name'} value={detailRole?.name ?? ''} onChange={(e) => {
+              setDetailRole({...detailRole, id: "ROLE_".concat(forceUpperSlug(e.target.value)), name: e.target.value})
+            }} />
+          </div>
+          <div className="modal-action">
+            <button className='btn bg-primary-2 text-white' onClick={onSubmit} disabled={loading}>{loading ? <FontAwesomeIcon icon={faSpinner} spin /> : <><FontAwesomeIcon icon={faSave} /> Save</>}</button>
+            <form method="dialog">
+              <button className="btn" disabled={loading}>Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
       <PageHeader page={"Role Access Page"} />
       <div className='my-5'>
-        <Section title={'Role Access'} additional={additionalRoleAccess}>
-          <div className='flex-wrap overflow-x-auto min-w-[300px]'>
+        <Section title={'Role Access'} >
+          <div className='flex-wrap overflow-x-auto min-w-[300px] py-5'>
             <div className='grid grid-cols-8 min-w-[1200px]'>
               <div className='col-span-2 px-2'>
-                <div className='px-5 py-5 border-l-2 border-l-white/20 cursor-pointer transition-all duration-200 ease-in-out'>
-                  <div className='relative'>
-                    <div className='absolute -ml-6 flex items-center h-full'>
-                      <div className='bg-white py-1 px-1 rounded-full' />
-                    </div>
-                    <span className='mb-5 block'>SUPERADMIN</span>
-                  </div>
-                  <div className='px-5 py-5 border-l-2 border-l-white/20 cursor-pointer transition-all duration-200 ease-in-out'>
-                    <div className='relative'>
-                      <div className='absolute -ml-6 flex items-center h-full'>
-                        <div className='bg-white py-1 px-1 rounded-full' />
-                      </div>
-                      <span className='mb-5 block'>SELLER</span>
-                    </div>
-                  </div>
-                  <div className='px-5 py-5 border-l-2 border-l-white/20 cursor-pointer transition-all duration-200 ease-in-out'>
-                    <div className='relative'>
-                      <div className='absolute -ml-6 flex items-center h-full'>
-                        <div className='bg-white py-1 px-1 rounded-full' />
-                      </div>
-                      <span className='mb-5 block'>CUSTOMER SERVICE</span>
-                    </div>
-                  </div>
-                  <div className='px-5 py-5 border-l-2 border-l-white/20 cursor-pointer transition-all duration-200 ease-in-out'>
-                    <div className='relative'>
-                      <div className='absolute -ml-6 flex items-center h-full'>
-                        <div className='bg-white py-1 px-1 rounded-full' />
-                      </div>
-                      <span className='mb-5 block'>ADMINISTRATOR</span>
-                    </div>
-                  </div>
-                  <div className='px-5 py-5 border-l-2 border-l-white/20 cursor-pointer transition-all duration-200 ease-in-out'>
-                    <div className='relative'>
-                      <div className='absolute -ml-6 flex items-center h-full'>
-                        <div className='bg-white py-1 px-1 rounded-full' />
-                      </div>
-                      <span className='mb-5 block'>REFUNDER</span>
-                    </div>
-                  </div>
+                <div className='overflow-y-auto w-full' style={{ maxHeight: '80vh' }}>
+                  {listRole?.map((val, i) =>
+                    <RoleItem val={val} key={val + i} action={showModal} setActive={setActiveRole} activeRole={activeRole} />
+                  )}
                 </div>
               </div>
               <div className='col-span-6 border-l-2 border-l-white/20 px-5'>
-                {
-                  accessList.map((val, i) => 
-                    <div className='border-b-2 border-b-white/20 last:border-b-0 my-5 px-5' key={val.id + i}>
-                      <span className='block'>
-                        {val?.id}
-                      </span>
-                      <div className='flex flex-wrap gap-x-10 gap-y-5 my-7'>
-                        {val?.accesses?.map((opt, j) => 
-                          <label className="label cursor-pointer justify-normal" key={opt.id + j}>
-                            <input type="checkbox" className="checkbox" checked={true} name='status' onChange={() => ''} disabled={false} />
-                            <span className="label-text pl-5">{opt?.id}</span>
-                          </label>
-                        )}
+                <div className='overflow-y-auto w-full' style={{ maxHeight: '80vh' }}>
+                  {
+                    accessList.map((val, i) =>
+                      <div className='border-b-2 border-b-white/20 last:border-b-0 my-5 px-5' key={val.id + i}>
+                        <span className='block'>
+                          {val?.id}
+                        </span>
+                        <div className='flex flex-wrap gap-x-10 gap-y-5 my-7'>
+                          {val?.accesses?.map((opt, j) =>
+                            <label className="label cursor-pointer justify-normal" key={opt.id + j}>
+                              <input type="checkbox" className="checkbox" checked={accessActive.findIndex(val => val?.id === opt?.id) !== -1} name='status' onChange={() => ''} disabled={false} />
+                              <span className="label-text pl-5">{opt?.id}</span>
+                            </label>
+                          )}
+                        </div>
                       </div>
-                    </div>
-                  )
-                }
+                    )
+                  }
+                </div>
               </div>
             </div>
           </div>
