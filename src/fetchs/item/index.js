2b5d9f599cdffffbc28a372f94d9ca11c47c39bf
agik.ap <agik2173@gmail.com>
2025-04-09 16:39:48 +0700

    add load variant external

--- src/fetchs/item/index.js
+++ src/fetchs/item/index.js
@@ -1,40 +1,53 @@
-import instance from "../../axios"
+import instance from "../../axios";
 
 export const GET_SyncItem = async (param, available, setLoading) => {
-  setLoading(true)
+  setLoading(true);
   try {
-    const response = await instance.get('/item/sync?isAvailable=' + available + '&keyword=' + param)
-    setLoading(false)
-    return response?.data?.data
+    const response = await instance.get(
+      "/item/sync?isAvailable=" + available + "&keyword=" + param
+    );
+    setLoading(false);
+    return response?.data?.data;
   } catch (e) {
-    setLoading(false)
-    throw e
+    setLoading(false);
+    throw e;
   }
-
-}
+};
 
 export const GET_Items = async (gameId, setLoading) => {
-  setLoading(true)
+  setLoading(true);
   try {
-    const response = await instance.get('/item/' + gameId)
-    setLoading(false)
-    return response?.data?.data
+    const response = await instance.get("/item/" + gameId);
+    setLoading(false);
+    return response?.data?.data;
   } catch (e) {
-    setLoading(false)
-    throw e
+    setLoading(false);
+    throw e;
   }
-
-}
+};
 
 export const POST_AssignItem = async (paylaod, gameId, setLoading) => {
-  setLoading(true)
+  setLoading(true);
   try {
-    const response = await instance.post('/item/assign/' + gameId, paylaod)
-    setLoading(false)
-    return response?.data?.data
+    const response = await instance.post("/item/assign/" + gameId, paylaod);
+    setLoading(false);
+    return response?.data?.data;
   } catch (e) {
-    setLoading(false)
-    throw e
+    setLoading(false);
+    throw e;
   }
+};
 
-}
+export const GET_LoadVariant = async (gameId, available, setLoading) => {
+  setLoading(true);
+  try {
+    const response = await instance.get(
+      `/item/load/variant/${gameId}?available=${available}`
+    );
+    setLoading(false);
+    return response?.data?.data;
+  } catch (e) {
+    setLoading(false);
+    throw e;
+  }
+};
